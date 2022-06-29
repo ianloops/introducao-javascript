f2f87be99eed78c8186719d753a352e787a6a492
ianloops <ianllopesc@gmail.com>
2022-06-29 19:30:13 -0300

    eventos implementados com 'addEventListener

--- contador/assets/scripts.js
+++ contador/assets/scripts.js
@@ -1,4 +1,4 @@
-var currentNumberWrapper = document.getElementById("currentNumber");
+/*var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
 
 function increment(){
@@ -8,4 +8,21 @@ function increment(){
 function decrement(){
     currentNumber--;
     currentNumberWrapper.innerHTML = currentNumber;
+}*/
+
+function increment(){
+    currentNumber++;
+    currentNumberWrapper.innerHTML = currentNumber;
 }
+function decrement(){
+    currentNumber--;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+var btnMais = document.getElementById("adicionar");
+var btnMenos = document.getElementById("subtrair");
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+btnMais.addEventListener("click", increment);
+btnMenos.addEventListener("click", decrement);
