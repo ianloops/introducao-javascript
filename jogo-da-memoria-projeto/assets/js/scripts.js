c5cfb62d50a4c0dd487872a7b45d109a5b81ec54
ianloops <ianllopesc@gmail.com>
2022-07-09 22:38:38 -0300

    Finalização projeto Jogo da memória

--- jogo-da-memoria-projeto/assets/js/scripts.js
+++ jogo-da-memoria-projeto/assets/js/scripts.js
@@ -1,7 +1,58 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
 function flipCard(){
-    this.classList.toggle('flip');
+    if (lockBoard) return;
+    if(this===firstCard) return;
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard=true;
+        firstCard= this;
+    } else{
+        secondCard = this;
+        hasFlippedCard = false;
+        checkForMatch();
+    }
+
 }
+
+function checkForMatch(){
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+    } else {
+        unflipCards();
+    }
+}
+
+function disableCards(){
+    lockBoard = true;
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCards(){
+    lockBoard = true;
+    setTimeout(() =>{
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');  
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard(){
+    [lockBoard, hasFlippedCard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    cards.forEach(card => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    });
+})();   
+
 cards.forEach((card) => {
     card.addEventListener('click', flipCard);
 });
